7ce2181400bcdd5413ae9776fdc04a896a1c686f
Eliza Brine <email": null>
2017-10-18 20:37:55 -0700

    Completes create employee route with errors, etc. No tests yet

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -5,10 +5,23 @@ class EmployeesController < ApplicationController
   end
 
   def create
+    @employee = Employee.new(employee_params)
+    if @employee.save
+      redirect_to "/"
+    else
+      @errors = @employee.errors.full_messages
+      render :new
+    end
   end
 
   private
-  def user_params
+  def employee_params
+    if params[:employee][:is_manager] == 'true'
+      params[:employee][:is_manager] = true
+    elsif params[:employee][:is_manager] == 'false'
+      params[:employee][:is_manager] = false
+    end
+    params.require(:employee).permit(:first_name, :last_name, :employee_id, :password, :is_manager)
   end
 
 
