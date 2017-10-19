a3d9f8eaa812673d517dbe1f97c9cdcf75289875
libby glomb <email": null>
2017-10-19 16:58:07 -0700

    remove quotes from show path

--- app/controllers/warehouses_controller.rb
+++ app/controllers/warehouses_controller.rb
@@ -13,7 +13,7 @@ class WarehousesController < ApplicationController
     warehouse = Warehouse.new(warehouse_params)
     warehouse.location_code = "#{warehouse.city.upcase[0..2]}-#{rand(10000..99999).to_s}"
     warehouse.save
-    redirect_to 'root_path'
+    redirect_to root_path
   end
 
   private
