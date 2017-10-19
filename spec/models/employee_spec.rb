d64eb1cfba4ed1f56be04e6aee3318b7708c8e82
Eliza Brine & Nick Van Deren <pair+ebrine.nickvanderen@devbootcamp.com>
2017-10-18 19:02:27 -0700

    Writes test for employee authenticate method"

--- spec/models/employee_spec.rb
+++ spec/models/employee_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
-RSpec.describe Employee, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Employee, type: :model do
+  describe "authenticate method" do
+    let(:employee) { Employee.create(employee_id: 123, password: "password") }
+    it "returns employee when password entered is correct" do
+      expect(employee.authenticate("password")).to eq Employee.find_by(employee_id: 123)
+    end
+  end
+
 end
