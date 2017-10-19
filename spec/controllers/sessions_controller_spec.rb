9148589f91bf3f7bcb52ee0093195662e1fe6527
Eliza Brine & Nick Van Deren <pair+ebrine.nickvanderen@devbootcamp.com>
2017-10-18 19:17:19 -0700

    Adds controller tests for unsuccessful employee login

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -13,5 +13,19 @@ describe SessionsController, type: :controller do
         expect(session[:user_id]).to be Employee.find_by(employee_id: 123).id
       end
     end
+    context "when employee enters incorrect login info" do
+      it "responds with status code of 200 (reloads login page)" do
+        post :create, { params: {employee_id: 1, password: "flamingo"} }
+        expect(response).to have_http_status 200
+      end
+      it "sets error message" do
+        post :create, { params: {employee_id: 1, password: "flamingo"} }
+        expect(assigns(:error)).to eq 'The employee number or password entered is incorrect.'
+      end
+      it ":user_id in session remains blank" do
+        post :create, { params: {employee_id: 1, password: "flamingo"} }
+        expect(session[:user_id]).to be nil
+      end
+    end
   end
 end
