cc6cdcb46d81f9576698dc787de592462199d5b2
Blaine Anderson <BlaineA97@Gmail.com>
2017-10-19 10:38:53 -0700

    commented unfinished test out

--- spec/controllers/warehouses_controller_spec.rb
+++ spec/controllers/warehouses_controller_spec.rb
@@ -8,11 +8,11 @@ RSpec.describe WarehousesController, type: :controller do
       get :new
       expect(response.status).to eq 302
     end
-    it "responds with status code 200 if manager_logged_in is true" do
-      employee = Employee.create(first_name: "Dr", last_name: "Rutherford", employee_id: 0000, password: "commissioner", is_manager: true, warehouse: warehouse)
-      get :new, { create: }
-      expect(response.status).to eq 200
-    end
+    # it "responds with status code 200 if manager_logged_in is true" do
+    #   employee = Employee.create(first_name: "Dr", last_name: "Rutherford", employee_id: 0000, password: "commissioner", is_manager: true, warehouse: warehouse)
+    #   get :new, { create: }
+    #   expect(response.status).to eq 200
+    # end
     it "assigns the last warehouse as @warehouse" do
       get :create, params: {warehouse: {city: "Phoenix"}}
       expect(assigns(:warehouse)).to eq Warehouse.last
@@ -21,7 +21,7 @@ RSpec.describe WarehousesController, type: :controller do
       get :new
       expect(response).to render_template(:new)
     end
-    
+
   end
 
 
