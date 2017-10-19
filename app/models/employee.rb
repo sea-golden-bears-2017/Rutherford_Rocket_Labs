d52e6112157f0bb9dd50e8bbffc504eb128dec63
Eliza Brine <email": null>
2017-10-19 13:43:51 -0700

    Creates associations btw parts and employees. Adds to seed file

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -2,6 +2,6 @@ class Employee < ApplicationRecord
   has_secure_password
 
   belongs_to :warehouse
-  has_many :parts, foreign_key: :removed_by_id
-  has_many :parts, foreign_key: :added_by_id
+  has_many :removed_parts, foreign_key: :removed_by_id, class_name: :Part
+  has_many :added_parts, foreign_key: :added_by_id, class_name: :Part
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-Warehouse.destroy_all
-Employee.destroy_all
+# Part.destroy_all
+# Employee.destroy_all
+# Warehouse.destroy_all
 
 warehouse = Warehouse.create(city: "Seattle", location_code: "SEA-00000")
 Warehouse.create(city: "Denver", location_code: "DEN-00000")
@@ -17,4 +17,4 @@ Warehouse.create(city: "Portland", location_code: "POR-00000")
 Employee.create(first_name: "Dr", last_name: "Rutherford", employee_id: 0000, password: "commissioner", is_manager: true, warehouse: warehouse)
 Employee.create(first_name: "Jules", last_name: "Dr", employee_id: 0001, password: "propulsion", is_manager: false, warehouse: warehouse)
 
-Part.create!(name: "o-ring", part_no: '35466', warehouse: warehouse, added_by_id: 2, removed: false, removed_by_id: 1)
+Part.create!(name: "o-ring", part_no: '35466', warehouse_id: 1, added_by_id: 2, removed: false)
