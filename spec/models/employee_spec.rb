f24aa8df977f525d1f2698cd37f928727ff1e290
Nick Van Deren <email": null>
2017-10-19 16:31:47 -0700

    employee spec tests

--- spec/models/employee_spec.rb
+++ spec/models/employee_spec.rb
@@ -1,13 +1,41 @@
 require 'rails_helper'
 
 describe Employee, type: :model do
-  let(:employee) { Employee.create(employee_id: 123, password: "password") }
+  let!(:employee) { Employee.create!( first_name: "Factory", last_name: "Girl", employee_id: 123, password: "password", is_manager: true, warehouse_id: 5) }
 
   describe "validations" do
-    it "is invalid when a first name is not entered"
-    it "is invalid when a last name is not entered"
-    it "is inavalid when an employee id is not entered"
-    it "is valid if one of the radio buttons is checked"
+    it "is invalid when a first name is not entered" do
+      employee.first_name = nil
+      expect(employee).to be_invalid
+    end
+
+    it "is invalid when a last name is not entered" do
+      employee.last_name = nil
+      expect(employee).to be_invalid
+    end
+
+    it "is inavalid when an employee id is not entered" do
+      employee.employee_id = nil
+      expect(employee).to be_invalid
+    end
+
+    it "is valid if manager" do
+      expect(employee.is_manager).to eq true
+    end
+
+    it "is valid if not a manager" do
+      employee.is_manager = false
+      expect(employee).to be_valid
+    end
+
+    it "invalid if not a manager or employee" do
+      employee.is_manager = nil
+      expect(employee).to be_invalid
+    end
+
+    it "is invalid if employee doesn't have a warehouse id" do
+      expect(employee.warehouse_id).to eq 5
+    end
   end
 
   describe "authenticate method" do
