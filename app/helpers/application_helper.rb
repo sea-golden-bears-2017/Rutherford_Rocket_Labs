2cd99d3f7b1e4650900e612e439a571263059350
libby glomb <email": null>
2017-10-18 18:41:23 -0700

    Add homes controller, custom home route and home view page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def logged_in?
+    return session[:employee_id] != nil
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,6 @@
 Rails.application.routes.draw do
+  resources :employees, only: [:index, :new, :create]
+
+  get "/home", :to => "homes#home"
+
 end
