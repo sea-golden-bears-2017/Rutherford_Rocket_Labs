e0f71edf9ef82ee8268bbfc42293813a98b57154
Eliza Brine <email": null>
2017-10-19 13:22:32 -0700

    Mgr can select warehouse for new employee from dropdown list

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,6 +10,9 @@ Warehouse.destroy_all
 Employee.destroy_all
 
 warehouse = Warehouse.create(city: "Seattle", location_code: "SEA-00000")
+Warehouse.create(city: "Denver", location_code: "DEN-00000")
+Warehouse.create(city: "Chicago", location_code: "CHI-00000")
+Warehouse.create(city: "Portland", location_code: "POR-00000")
 
 Employee.create(first_name: "Dr", last_name: "Rutherford", employee_id: 0000, password: "commissioner", is_manager: true, warehouse: warehouse)
 Employee.create(first_name: "Jules", last_name: "Dr", employee_id: 0001, password: "propulsion", is_manager: false, warehouse: warehouse)
