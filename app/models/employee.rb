f2e2c3c273b30e0c0d13feb8466b4b70a588f79b
Nick Van Deren <email": null>
2017-10-19 11:40:20 -0700

    creates spec to-list, adds employee validations

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,4 +1,6 @@
 class Employee < ApplicationRecord
+
+  validates :first_name, :last_name, :employee_id, presence: true
   has_secure_password
 
   belongs_to :warehouse
--- spec/models/employee_spec.rb
+++ spec/models/employee_spec.rb
@@ -1,8 +1,16 @@
 require 'rails_helper'
 
 describe Employee, type: :model do
+  let(:employee) { Employee.create(employee_id: 123, password: "password") }
+
+  describe "validations" do
+    it "is invalid when a first name is not entered"
+    it "is invalid when a last name is not entered"
+    it "is inavalid when an employee id is not entered"
+    it "is valid if one of the radio buttons is checked"
+  end
+
   describe "authenticate method" do
-    let(:employee) { Employee.create(employee_id: 123, password: "password") }
     it "returns employee when password entered is correct" do
       expect(employee.authenticate("password")).to eq Employee.find_by(employee_id: 123)
     end
