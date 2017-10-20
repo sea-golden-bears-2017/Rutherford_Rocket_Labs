21ef860a48f23045b84e14935ad388005b544c43
Nick Van Deren <email": null>
2017-10-20 12:37:08 -0700

    orders controller spec tests

--- spec/controllers/orders_controller_spec.rb
+++ spec/controllers/orders_controller_spec.rb
@@ -5,7 +5,11 @@ describe OrdersController, type: :controller do
     before(:each) do
       get :new
     end
-    it 'responds with a 200 status code'
-    it 'renders a new orders template'
+    it 'responds with a 200 status code' do
+      expect(response.status).to eq 200
+    end
+    it 'renders a new orders template' do
+      expect(response).to render_template(:new)
+    end
   end
 end
