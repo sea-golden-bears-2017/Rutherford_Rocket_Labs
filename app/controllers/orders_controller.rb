0fce89eee9482085eeac300a75e074c146ca8861
Nick Van Deren <email": null>
2017-10-20 16:56:07 -0700

    dummy order page

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,23 +3,12 @@ class OrdersController < ApplicationController
   end
 
   def create
-    if order.save
-      4/0
-      redirect_to :show
-    else
-      @errors = order.errors.full_messages
-      redirect_to :new
-    end
+    redirect_to home_path
   end
 
   def show
   end
 
   def new
-  end
-
-
-  def order_params
-    params.require(:order).permit(:name, :description, :warehouse_id, :part_id, :quantity)
   end
 end
