87aaceea9069e88b92a7193eddb4bf87bd90315b
Blaine Anderson <BlaineA97@Gmail.com>
2017-10-21 16:42:13 -0700

    added index success passing test

--- spec/controllers/parts_controller_spec.rb
+++ spec/controllers/parts_controller_spec.rb
@@ -1,11 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe PartsController, type: :controller do
+  let!(:warehouse) {Warehouse.create!(city: "Phoenix", location_code: 1)}
+  let!(:employee) {Employee.create(first_name: "Dr", last_name: "Rutherford", employee_id: 0000, password: "commissioner", is_manager: true, warehouse: warehouse)}
 
 context "GET parts#index" do
   it "responds with status code 302 if logged_in? is false" do
     get :index
     expect(response.status).to eq 302
+  end
+  it "responds with status code 200 if logged_in? is true" do
+    session[:user_id] = employee.id
+    get :index
+    expect(response.status).to eq 200
   end
 end
 
