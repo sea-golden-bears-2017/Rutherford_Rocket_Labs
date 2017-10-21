c626522c0b1a4ed44fe063f02861a50eefecc5a1
Blaine Anderson <BlaineA97@Gmail.com>
2017-10-21 16:46:09 -0700

    added two passing tests for :new

--- spec/controllers/parts_controller_spec.rb
+++ spec/controllers/parts_controller_spec.rb
@@ -4,17 +4,33 @@ RSpec.describe PartsController, type: :controller do
   let!(:warehouse) {Warehouse.create!(city: "Phoenix", location_code: 1)}
   let!(:employee) {Employee.create(first_name: "Dr", last_name: "Rutherford", employee_id: 0000, password: "commissioner", is_manager: true, warehouse: warehouse)}
 
-context "GET parts#index" do
-  it "responds with status code 302 if logged_in? is false" do
-    get :index
-    expect(response.status).to eq 302
+  context "GET parts#index" do
+    it "responds with status code 302 if logged_in? is false" do
+      get :index
+      expect(response.status).to eq 302
+    end
+    it "responds with status code 200 if logged_in? is true" do
+      session[:user_id] = employee.id
+      get :index
+      expect(response.status).to eq 200
+    end
   end
-  it "responds with status code 200 if logged_in? is true" do
-    session[:user_id] = employee.id
-    get :index
-    expect(response.status).to eq 200
+
+  context "GET parts#new" do
+    it 'renders the :new template' do
+      get :new
+      expect(response).to render_template :new
+    end
+    it 'responds with a status of 200' do
+      get :new
+      expect(response.status).to eq 200
+    end
   end
-end
+
+
+
+  context "GET parts#new"
+
 
 
 end
