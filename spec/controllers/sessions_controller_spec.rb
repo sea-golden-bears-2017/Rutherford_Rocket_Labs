b49bfcd35fc4f35a03e82b247609c326d51efa2c
Blaine Anderson <BlaineA97@Gmail.com>
2017-10-21 20:08:14 -0700

    added 2 tests for destroy route: returns a status of 302 when destory route is called & :user_id is cleared when destroy route is called

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -27,6 +27,14 @@ describe SessionsController, type: :controller do
         post :create, { params: {employee_id: 1, password: "flamingo"} }
         expect(session[:user_id]).to be nil
       end
+      it 'returns a status of 302 when destory route is called' do
+        delete :destroy
+        expect(response.status).to be 302
+      end
+      it ':user_id is cleared when destroy route is called' do
+        delete :destroy
+        expect(session).to be_empty 
+      end
     end
   end
 end
