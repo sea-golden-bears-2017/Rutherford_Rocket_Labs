bc140dd143d3c184332ef0ec57ff30a42cf2ea76
libby glomb <eglombhgai@gmail.com>
2017-10-21 22:18:10 -0700

    Revert "Transfer styles"

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :employees, only: [:new, :create, :confirmation]
   resources :warehouses, only: [:new, :create, :confirmation]
-  resources :parts, only: [:index, :create, :new, :edit, :remove, :new_line, :show]
+  resources :parts, only: [:index, :create, :new, :remove, :new_line, :show]
   resources :orders, only: [:index, :new, :create, :show, :confirmation, :edit, :update]
 
   root to: 'homes#show'
