29f9e4589f78806cb57dc48c6638f9d66656669d
Eliza Brine <email": null>
2017-10-22 17:05:29 -0700

    Adds line to config to serve static assets

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,7 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+config.serve_static_assets = true
 module RutherfordInventory
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
